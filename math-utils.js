0f3105f292e6bb62de9d6668dc36d9593e5dac4c
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2021-11-05 14:52:52 -0700

    added assets, styled display in css, styled boxes

--- math-utils.js
+++ math-utils.js
@@ -1,19 +1,15 @@
 export function add(firstNum, secondNum) {
-    
     return firstNum + secondNum;
 };
 
 export function sub(firstNum, secondNum) {
-
     return firstNum - secondNum;
 };
 
 export function mult(firstNum, secondNum) {
-
     return firstNum * secondNum;
 };
 
 export function divide(firstNum, secondNum) {
-
     return firstNum / secondNum;
 };
