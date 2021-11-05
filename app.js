26577fc01b6f127c1e4a35d8b6e573c920fb67b0
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2021-11-05 12:13:42 -0700

    created getElems for all inputs, buttons, results

---
+++ app.js
@@ -0,0 +1,25 @@
+const addInput1 = document.getElementById('add-input-1');
+const addInput2 = document.getElementById('add-input-2');
+const addButton = document.getElementById('add-button');
+const addResult = document.getElementById('add-result');
+
+
+
+const subInput1 = document.getElementById('sub-input-1');
+const subInput2 = document.getElementById('sub-input-2');
+const subButton = document.getElementById('sub-button');
+const subResult = document.getElementById('sub-result');
+
+
+
+const multInput1 = document.getElementById('mult-input-1');
+const multInput2 = document.getElementById('mult-input-2');
+const multButton = document.getElementById('mult-button');
+const multResult = document.getElementById('mult-result');
+
+
+
+const divideInput1 = document.getElementById('divide-input-1');
+const divideInput2 = document.getElementById('divide-input-2');
+const divideButton = document.getElementById('divide-button');
+const divideResult = document.getElementById('divide-result');
