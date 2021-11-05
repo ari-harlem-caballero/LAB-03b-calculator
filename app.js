5e9511838d85b3c671749e5d6d132d429d283299
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2021-11-05 13:33:19 -0700

    created add-button event listener, logged

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
+import { add, sub, mult, divide } from './math-utils.js'
+
 const addInput1 = document.getElementById('add-input-1');
 const addInput2 = document.getElementById('add-input-2');
 const addButton = document.getElementById('add-button');
 const addResult = document.getElementById('add-result');
-
-
 
 const subInput1 = document.getElementById('sub-input-1');
 const subInput2 = document.getElementById('sub-input-2');
 const subButton = document.getElementById('sub-button');
 const subResult = document.getElementById('sub-result');
 
-
-
 const multInput1 = document.getElementById('mult-input-1');
 const multInput2 = document.getElementById('mult-input-2');
 const multButton = document.getElementById('mult-button');
 const multResult = document.getElementById('mult-result');
 
-
-
 const divideInput1 = document.getElementById('divide-input-1');
 const divideInput2 = document.getElementById('divide-input-2');
 const divideButton = document.getElementById('divide-button');
 const divideResult = document.getElementById('divide-result');
+
+addButton.addEventListener('click', () => {
+
+  const number1 = +addInput1.value;
+  const number2 = +addInput2.value;
+
+  const result = add(number1, number2);
+});
--- math-utils.js
+++ math-utils.js
@@ -1,19 +1,19 @@
 export function add(firstNum, secondNum) {
     
-    // return firstNum + secondNum;
+    return firstNum + secondNum;
 };
 
 export function sub(firstNum, secondNum) {
 
-    // return firstNum - secondNum;
+    return firstNum - secondNum;
 };
 
 export function mult(firstNum, secondNum) {
 
-    // return firstNum * secondNum;
+    return firstNum * secondNum;
 };
 
 export function divide(firstNum, secondNum) {
 
-    // return firstNum / secondNum;
+    return firstNum / secondNum;
 };
